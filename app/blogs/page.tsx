7ea79af28eae2df1519fc2f584b8bc1bd65f185e
GURURAJ115 <gururajsnaik115@gmail.com>
2025-02-14 21:07:18 +0530

    routing and SEO in nextjs

---
+++ app/blogs/page.tsx
@@ -0,0 +1,7 @@
+export default function Blogs(){
+    return <div>
+        Learn Recoil / redux from the best platform on the world
+    </div>
+} 
+
+//this will go directly into the html , will not follow waterfall model like react, nextjs does serverside rendering and put this in html and then sends it to the browser, which inturn leads in high rating of SEo.
