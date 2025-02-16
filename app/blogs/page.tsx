8b340228adbdba62562b1f9d31b9a5ed0a455a2f
GURURAJ115 <gururajsnaik115@gmail.com>
2025-02-16 16:15:38 +0530

    Server Side Rendering of todos

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,7 +1,27 @@
-export default function Blogs(){
+import axios from "axios";
+
+async function getBlogs() {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/todos")
+    return response.data;
+}
+
+export default async function Blogs(){
+    const blogs = await getBlogs();
+    
     return <div>
-        Learn Recoil / redux from the best platform on the world
+        {blogs.map((blog:ITodo) => <Todo title={blog.title} completed={blog.completed} />)}
     </div>
 } 
 
 //this will go directly into the html , will not follow waterfall model like react, nextjs does serverside rendering and put this in html and then sends it to the browser, which inturn leads in high rating of SEo.
+
+interface ITodo{
+    title: string;
+    completed: boolean;
+}
+
+function Todo({title, completed}: ITodo){
+    return <div>
+        {title} {completed ? "done!":"not done"}
+    </div>
+}
