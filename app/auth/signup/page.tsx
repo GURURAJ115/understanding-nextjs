c66c96404b012f2d13d81bba0f2ce7f46892a031
GURURAJ115 <gururajsnaik115@gmail.com>
2025-02-21 10:19:36 +0530

    Backend Connectivity

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,4 +1,13 @@
+"use client"
+import axios from "axios";
+import { useState } from "react";
+
 export default function Signup() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+
+
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
         <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
@@ -9,25 +18,38 @@ export default function Signup() {
                         </div>
                     </div>
                     <div className="pt-2">
-                        <LabelledInput label="Username" placeholder="harkirat@gmail.com" />
-                        <LabelledInput label="Password" type={"password"} placeholder="123456" />
-                        <button type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign Up</button>
+                        <LabelledInput label="Username" type="text" placeholder="harkirat@gmail.com" onChange={setUsername}/>
+                        <LabelledInput label="Password" type={"password"} placeholder="123456" onChange={setPassword}/>
+                        <button type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2" onClick={()=>{
+                            axios.post("http://localhost:3000/api/v1/signup",{
+                                username,
+                                password
+                            })
+                        }}>Sign Up</button>
                     </div>
                 </div>
             </a>
         </div>
     </div>
 }
-
 interface LabelledInputType {
     label: string;
     placeholder: string;
     type?: string;
+    onChange: (value: string) => void;
 }
 
-function LabelledInput({ label, placeholder, type }: LabelledInputType) {
-    return <div>
-        <label className="block mb-2 text-sm text-black font-semibold pt-4">{label}</label>
-        <input type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
-    </div>
+function LabelledInput({ label, placeholder, type, onChange }: LabelledInputType) {
+    return (
+        <div>
+            <label className="block mb-2 text-sm text-black font-semibold pt-4">{label}</label>
+            <input 
+                type={type || "text"} 
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" 
+                placeholder={placeholder} 
+                required 
+                onChange={e => onChange(e.target.value)}
+            />
+        </div>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <Button>Sign Up</Button>
-      <Button>Sign In</Button>
+
+      <div className="text-lg w-screen h-screen flex items-center justify-center">
+      <div>
+        Todo Application
+        <br />
+        <Link className="text-md border m-2" href="/auth/signin">Sign in to Todo app</Link>
+        <br />
+        <Link className="text-md border m-2" href="/auth/signup">Sign up to Todo app</Link>
+      </div>
     </div>
+    
   );
 }
 
-function Button({children}){
-  return <button>{children}</button>
-}
+// function Button({children}){
+//   return <button>{children}</button>
+// }
