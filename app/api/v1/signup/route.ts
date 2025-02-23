da2ba0b8d71da03fab83799d8b0cffac218301fe
GURURAJ115 <gururajsnaik115@gmail.com>
2025-02-23 11:59:19 +0530

    To stop repeated db client calls in prisma , use prisma singleton method

--- app/api/v1/signup/route.ts
+++ app/api/v1/signup/route.ts
@@ -1,13 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
-
-const prismaClient = new PrismaClient();
+// import { PrismaClient } from "@prisma/client";
+import prisma from "@/app/lib/db";
+// const prismaClient = new PrismaClient();
 
 export async function POST(req: NextRequest){
 
     const data = await req.json();
     
-    await prismaClient.user.create({
+    await prisma.user.create({
         data:{
             username: data.username,
             password: data.password
