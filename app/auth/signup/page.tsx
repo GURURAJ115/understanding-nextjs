a13ec4ce5fb9eaa371fef39bdfd76e7d4ce196e3
GURURAJ115 <gururajsnaik115@gmail.com>
2025-02-23 11:50:14 +0530

    DB connectivity

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function Signup() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-
+    const router = useRouter();
 
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
@@ -20,11 +21,12 @@ export default function Signup() {
                     <div className="pt-2">
                         <LabelledInput label="Username" type="text" placeholder="harkirat@gmail.com" onChange={setUsername}/>
                         <LabelledInput label="Password" type={"password"} placeholder="123456" onChange={setPassword}/>
-                        <button type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2" onClick={()=>{
-                            axios.post("http://localhost:3000/api/v1/signup",{
+                        <button type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2" onClick={async ()=>{
+                            await axios.post("http://localhost:3000/api/v1/signup",{
                                 username,
                                 password
                             })
+                            router.push("/auth/signin")
                         }}>Sign Up</button>
                     </div>
                 </div>
